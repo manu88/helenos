e68716fe1bc502dedc70fc1703ba45854bf09975
Manuel Deneu <manueldeneu@gmail.com>
2018-06-06 20:32:33 +0200

    Added AKAlertWindow

---
+++ uspace/lib/AppKit/AKAlertWindow.c
@@ -0,0 +1,8 @@
+//
+//  AKAlertWindow.c
+//  
+//
+//  Created by Manuel Deneu on 06/06/2018.
+//
+
+#include "AKAlertWindow.h"
---
+++ uspace/lib/AppKit/AKAlertWindow.h
@@ -0,0 +1,13 @@
+//
+//  AKAlertWindow.h
+//  
+//
+//  Created by Manuel Deneu on 06/06/2018.
+//
+
+#ifndef AKAlertWindow_h
+#define AKAlertWindow_h
+
+#include <stdio.h>
+
+#endif /* AKAlertWindow_h */
